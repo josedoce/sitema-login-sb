2e4df6075200188a038bb727cea60e56483a0e34
josedoce <josesamya@gmail.com>
2021-08-14 11:11:40 -0300

    testes: não pretendo por enquanto...

---
+++ src/test/java/com/springseguro/eumesmo/EumesmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springseguro.eumesmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EumesmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
