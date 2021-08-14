4fd36c630c476597bac5af664c1402b2ae8626bc
josedoce <josesamya@gmail.com>
2021-08-14 11:24:26 -0300

    ponto de partida do spring

---
+++ src/main/java/com/springseguro/eumesmo/EumesmoApplication.java
@@ -0,0 +1,13 @@
+package com.springseguro.eumesmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumesmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumesmoApplication.class, args);
+	}
+
+}
